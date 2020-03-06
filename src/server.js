8ae60975207b1bf5aded510c1e7d888b8108496a
Gustavo Fonseca <gustavofonsec94@gmail.com>
2020-03-06 08:10:16 -0300

    Add docker-compose files

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const HttpStatus = require('http-status-codes')
 const bodyParser = require('body-parser')
+const logger = require('./logger')
 
 app.use(bodyParser.json())
 app.use(require('./routes'))
@@ -10,7 +11,7 @@ app.use(require('./routes'))
 // eslint-disable-next-line
 app.use((err, req, res, next) => {
   if (process.env.NODE_ENV === 'dev') {
-    console.log(err)
+    logger.debug(err)
     return res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR).json({ error: err.message })
   }
 
